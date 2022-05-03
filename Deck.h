7d04a01f97973c3e064bbfd20e13a313e8413bbd
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-05-03 19:51:46 +0600

    Deck class created.

---
+++ Deck.h
@@ -0,0 +1,18 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include "Hand.h"
+#include "GenericPlayer.h"
+
+class Deck: public Hand
+{
+public:
+  Deck();
+  virtual ~Deck();
+  //creates a deck of 52 cards
+  void Populate();
+  void Shuffle();
+  void Deal(Hand& aHand);
+  void AdditionalCards(GenericPlayer& aGenericPlayer);
+};
+#endif
