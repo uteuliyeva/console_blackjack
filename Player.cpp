e6a53035046e298daac8ddb75346a169d235d313
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-05-03 16:23:14 +0600

    Player class created.

---
+++ Player.cpp
@@ -0,0 +1,31 @@
+#include "Player.h"
+
+Player::Player(const string& name):
+  GenericPlayer(name)
+{}
+
+Player::~Player()
+{}
+
+bool Player::IsHitting() const
+{
+  cout<<m_Name<<", do you want a hit? (Y/N): ";
+  char answer;
+  cin>>answer;
+  return (answer=='y'||answer=='Y');
+}
+
+void Player::Win() const
+{
+  cout<<m_Name<<" wins."<<endl;
+}
+
+void Player::Lose() const
+{
+  cout<<m_Name<<" loses."<<endl;
+}
+
+void Player::Push() const
+{
+  cout<<m_Name<<" pushes."<<endl;
+}
---
+++ Player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include "GenericPlayer.h"
+
+class Player: public GenericPlayer
+{
+public:
+  Player(const string& name="");
+  virtual ~Player();
+
+  //whether the player wants another card
+  virtual bool IsHitting() const;
+
+  //next functions announce that the player wins/loses/pushes
+  void Win() const;
+  void Lose() const;
+  void Push() const;
+};
+#endif
