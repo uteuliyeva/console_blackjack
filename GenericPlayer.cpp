edac290a00c3214f8ddfbc208716b11b8b19793f
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-05-04 10:50:14 +0600

    Operator << overload function definitions added to Card and Generic Player classes.

--- GenericPlayer.cpp
+++ GenericPlayer.cpp
@@ -20,3 +20,25 @@ void GenericPlayer::Bust() const
 {
   cout<<m_Name<<" busts."<<endl;
 }
+
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+  os<<aGenericPlayer.m_Name<<":\t";
+  vector<Card*>::const_iterator pCard;
+  if(!aGenericPlayer.m_Cards.empty())
+  {
+    for (pCard=aGenericPlayer.m_Cards.begin();pCard!=aGenericPlayer.m_Cards.end();++pCard)
+    {
+      os<<*(*pCard)<<"\t";
+    }
+    if (aGenericPlayer.GetTotal()!=0)
+    {
+      cout<<"("<<aGenericPlayer.GetTotal()<<")"<<endl;
+    }
+  }
+  else
+  {
+    os<<"<empty>";
+  }
+  return os;
+}
